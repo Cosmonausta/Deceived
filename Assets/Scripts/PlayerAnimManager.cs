d4cf810f907016c9707902e6dc20f87fbd7ebe60
Joshua Ásberg O'Neill <joshoneill94@gmail.com>
2017-12-08 20:07:07 +0000

    Initital commit

---
+++ Assets/Scripts/PlayerAnimManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerAnimManager : MonoBehaviour {
+
+	private Animator anim;
+	public NavMeshAgent navMeshAgent;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		navMeshAgent = GetComponent<NavMeshAgent>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		anim.SetFloat ("PlayerVelocity", navMeshAgent.velocity.magnitude);
+	}
+}
---
+++ Assets/Scripts/PlayerAudioManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayerAudioManager : MonoBehaviour {
+	public AudioSource sfxAudioS;
+
+	public AudioClip[] hardSteps;
+
+	public float distToGround = 1.3f;
+	RaycastHit hitInfo;
+
+	bool Grounded()
+	{
+		return Physics.Raycast (transform.position + Vector3.up * 0.5f, -Vector3.up, out hitInfo, distToGround);
+	}
+
+	public void Footsteps()
+	{
+		if (Grounded ()) 
+		{
+			int r = Random.Range (0, hardSteps.Length);
+			switch (hitInfo.transform.GetComponent<Collider> ().tag) 
+			{
+				case "HardFloor":
+					sfxAudioS.PlayOneShot(hardSteps[r]);
+					break;
+				default:
+					sfxAudioS.PlayOneShot(hardSteps[r]);
+					break;
+			}
+		}
+	}
+
+}
